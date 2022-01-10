4294d6db5121147609bef2aaeb16839bda63697e
daniel espino gomis <daniel12058@iesmarenostrum.com>
2022-01-10 19:12:36 +0100

    55
    55

--- Servidor Práctica 3/MvcSoporte/MvcSoporte/Controllers/HomeController.cs
+++ Servidor Práctica 3/MvcSoporte/MvcSoporte/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MvcSoporte.Data;
 using MvcSoporte.Models;
 using System;
 using System.Collections.Generic;
@@ -9,19 +11,35 @@ using System.Threading.Tasks;
 
 namespace MvcSoporte.Controllers
 {
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcSoporteContexto _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MvcSoporteContexto context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            // Busca empleado correspondiente al usuario actual. Si existe 
+            // va a View y en caso contrario, va a crear el empleado.
+            string emailUsuario = User.Identity.Name;
+            Empleado empleado = _context.Empleados.Where(e => e.Email == emailUsuario)
+            .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            empleado == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
             return View();
         }
+
+        [Authorize(Roles = "Usuario")]
 
         public IActionResult Privacy()
         {
--- Servidor Práctica 3/MvcSoporte/MvcSoporte/Controllers/TipoAveriasController.cs
+++ Servidor Práctica 3/MvcSoporte/MvcSoporte/Controllers/TipoAveriasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using MvcSoporte.Models;
 
 namespace MvcSoporte.Controllers
 {
+    [Authorize(Roles = "Administrador")]
     public class TipoAveriasController : Controller
     {
         private readonly MvcSoporteContexto _context;
@@ -20,6 +22,7 @@ namespace MvcSoporte.Controllers
         }
 
         // GET: TipoAverias
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.TipoAverias.ToListAsync());
