976ec4e98bb9689c51137284491ecd16ca7fa428
daniel espino gomis <daniel12058@iesmarenostrum.com>
2022-01-12 16:58:53 +0100

    fin

--- Servidor Práctica 3/MvcSoporte/MvcSoporte/Controllers/UsuariosController.cs
+++ Servidor Práctica 3/MvcSoporte/MvcSoporte/Controllers/UsuariosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MvcSoporte.Data;
@@ -14,9 +16,12 @@ namespace MvcSoporte.Controllers
     public class UsuariosController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public UsuariosController(ApplicationDbContext context)
+        private readonly UserManager<IdentityUser> _userManager;
+        public UsuariosController(ApplicationDbContext context,
+ UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
@@ -31,5 +36,32 @@ namespace MvcSoporte.Controllers
                            };
             return View(await usuarios.ToListAsync());
         }
+        // GET: usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: Usuarios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Email,Password")]
+         RegisterModel.InputModel model)
+        {
+            // Se crea el nuevo usuario
+            var user = new IdentityUser();
+            user.UserName = model.Email;
+            user.Email = model.Email;
+            string usuarioPWD = model.Password;
+            var result = await _userManager.CreateAsync(user, usuarioPWD);
+
+            // Se asigna el rol de "Administrador" al usuario
+            if (result.Succeeded)
+            {
+                var result1 = await _userManager.AddToRoleAsync(user, "Administrador");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
     }
 }
